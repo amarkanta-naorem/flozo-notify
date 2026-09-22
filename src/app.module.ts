ce87654ac66b37992ee73663b5c1bc4115309944
amarkanta-naorem <naoremamarkanta@gmail.com>
2026-09-22 11:46:40 +0530

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller.js';
+// import { AppService } from './app.service.js';
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
+
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+})
+export class AppModule {}
